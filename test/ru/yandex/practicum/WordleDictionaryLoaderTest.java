77351fe14bb172bdd4356cc5b06e8b1cc62c8443
Ivan Ivashkin <ivan.ivashkin@ax****.***>
2026-02-01 13:59:01 +0300

    Исправление теста testLoadDictionaryWithInvalidPath

--- test/ru/yandex/practicum/WordleDictionaryLoaderTest.java
+++ test/ru/yandex/practicum/WordleDictionaryLoaderTest.java
@@ -104,8 +104,8 @@ public class WordleDictionaryLoaderTest {
         assertThrows(DictionaryFileNotFoundException.class, () ->
                 runtimeExceptionDictionaryLoader.loadDictionary("test.jpg"));
 
-        assertTrue(getLogInfo().contains("Некорректный путь к файлу."),
-                "Должно быть /" + getLogInfo()
+        assertTrue(getLogInfo().contains("Файла словаря test.jpg не существует"),
+                "Должно быть: " + getLogInfo()
         );
     }
 
